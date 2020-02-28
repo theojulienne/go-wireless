1331708dfa6e5b013c82f28487436675da407472
Robert McLeod <robert@penguinpowernz.co.nz>
2020-02-28 23:03:18 +1300

    update README.md with api info

--- api/routes.go
+++ api/routes.go
@@ -9,7 +9,7 @@ func SetupRoutes(r gin.IRouter) {
 
 	r.GET("/interfaces/:iface/aps", listAccesPoints)
 
-	r.GET("/interfaces/:iface/networks", notImplemented)
+	r.GET("/interfaces/:iface/networks", listNetworks)
 	r.POST("/interfaces/:iface/networks", notImplemented)
 	r.PUT("/interfaces/:iface/networks/:id_or_idstr", notImplemented)
 	r.GET("/interfaces/:iface/networks/:id_or_idstr", notImplemented)

b2660cf14f185e6fa82531f11720c526e7ef49a0
Robert McLeod <robert@penguinpowernz.co.nz>
2020-02-28 23:03:18 +1300

    add list networks to the API

--- api/handlers.go
+++ api/handlers.go
@@ -9,6 +9,10 @@ func notImplemented(c *gin.Context) {
 	c.AbortWithStatus(501)
 }
 
+func json(err error) map[string]string {
+	return map[string]string{"error": err.Error()}
+}
+
 func listInterfaces(c *gin.Context) {
 	c.JSON(200, wireless.Interfaces())
 }
@@ -20,9 +24,9 @@ func listAccesPoints(c *gin.Context) {
 		c.Error(err)
 		c.AbortWithStatusJSON(500, json(err))
 	}
+	defer wc.Close()
 
 	aps, err := wc.Scan()
-
 	if err != nil {
 		c.Error(err)
 		c.AbortWithStatusJSON(500, json(err))
@@ -31,6 +35,20 @@ func listAccesPoints(c *gin.Context) {
 	c.JSON(200, aps)
 }
 
-func json(err) map[string]string {
-	return map[string]string{"error": err.Error()}
+func listNetworks(c *gin.Context) {
+	iface := c.Param("iface")
+	wc, err := wireless.NewClient(iface)
+	if err != nil {
+		c.Error(err)
+		c.AbortWithStatusJSON(500, json(err))
+	}
+	defer wc.Close()
+
+	nets, err := wc.Networks()
+	if err != nil {
+		c.Error(err)
+		c.AbortWithStatusJSON(500, json(err))
+	}
+
+	c.JSON(200, nets)
 }

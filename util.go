9cf7100da95679108b2cb48239e02f5f373ed930
Robert McLeod <robert@penguinpowernz.co.nz>
2020-02-29 00:35:56 +1300

    move utils funcs into util file

---
+++ util.go
@@ -0,0 +1,103 @@
+package wireless
+
+import (
+	"bytes"
+	"encoding/csv"
+	"net"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+func parseNetwork(b []byte) ([]Network, error) {
+	i := bytes.Index(b, []byte("\n"))
+	if i > 0 {
+		b = b[i:]
+	}
+
+	r := csv.NewReader(bytes.NewReader(b))
+	r.Comma = '\t'
+	r.FieldsPerRecord = 4
+
+	recs, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	nts := []Network{}
+	for _, rec := range recs {
+		id, err := strconv.Atoi(rec[0])
+		if err != nil {
+			return nil, errors.Wrap(err, "parse id")
+		}
+
+		nts = append(nts, Network{
+			ID:    id,
+			SSID:  rec[1],
+			BSSID: rec[2],
+			Flags: parseFlags(rec[3]),
+		})
+	}
+
+	return nts, nil
+}
+
+func parseFlags(s string) []string {
+	s = strings.TrimPrefix(s, "[")
+	s = strings.TrimSuffix(s, "]")
+
+	return strings.Split(s, "][")
+}
+
+func parseAP(b []byte) ([]AP, error) {
+	i := bytes.Index(b, []byte("\n"))
+	if i > 0 {
+		b = b[i:]
+	}
+
+	r := csv.NewReader(bytes.NewReader(b))
+	r.Comma = '\t'
+	r.FieldsPerRecord = 5
+
+	recs, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	aps := []AP{}
+	for _, rec := range recs {
+		bssid, err := net.ParseMAC(rec[0])
+		if err != nil {
+			return nil, errors.Wrap(err, "parse mac")
+		}
+
+		fr, err := strconv.Atoi(rec[1])
+		if err != nil {
+			return nil, errors.Wrap(err, "parse frequency")
+		}
+
+		ss, err := strconv.Atoi(rec[2])
+		if err != nil {
+			return nil, errors.Wrap(err, "parse signal strength")
+		}
+
+		aps = append(aps, AP{
+			BSSID:          bssid,
+			SSID:           rec[4],
+			Frequency:      fr,
+			SignalStrength: ss,
+			Flags:          parseFlags(rec[3]),
+		})
+	}
+
+	return aps, nil
+}
+
+func quote(s string) string {
+	return `"` + s + `"`
+}
+
+func itoa(i int) string {
+	return strconv.Itoa(i)
+}

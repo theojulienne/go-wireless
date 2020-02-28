eb981cb3ecdf104672880f6fa7b3ad8b2f1f15b1
Robert McLeod <robert@penguinpowernz.co.nz>
2020-02-28 22:50:03 +1300

    add a method to find wireless interfacs on the current system

---
+++ interfaces.go
@@ -0,0 +1,27 @@
+package wireless
+
+import (
+	"os"
+	"path"
+	"path/filepath"
+)
+
+// Interfaces is a shortcut to the best known method for gathering the wireless
+// interfaces from the current system
+var Interfaces = SysFSInterfaces
+
+// SysFSInterfaces returns the wireless interfaces found in the SysFS (/sys)
+func SysFSInterfaces() []string {
+	s := []string{}
+	base := "/sys/class/net"
+	matches, _ := filepath.Glob(path.Join(base, "*"))
+
+	//  look for the wireless folder in each interfces directory to determine if it is a wireless device
+	for _, iface := range matches {
+		if _, err := os.Stat(path.Join(iface, "wireless")); err != nil {
+			s = append(s, path.Base(iface))
+		}
+	}
+
+	return s
+}

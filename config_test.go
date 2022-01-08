58650b5066c83c55d44752878cc996185923ffeb
Derek Ludwig <derek.s.ludwig@gmail.com>
2022-01-07 17:35:17 -0800

    test: make tests explicit

--- config_test.go
+++ config_test.go
@@ -5,22 +5,50 @@ import (
 	"testing"
 )
 
-var configTests = []string{
-	"hello world",
-	`hello\world`,
-	"hello\tworld",
-	"Ë \" ' \\ 😅",
+func TestConfigRoundTripPlain(t *testing.T) {
+	tt := "hello world"
+	in := configWriteString(tt)
+	out, err := configParseString(in)
+	if err != nil {
+		t.Errorf("unexpected error in configParseString, %s", err)
+	}
+	if out != tt {
+		t.Errorf("configParseString returned %s (wanted %s)", strconv.QuoteToASCII(out), strconv.QuoteToASCII(tt))
+	}
 }
 
-func TestConfigRoundTrip(t *testing.T) {
-	for _, tt := range configTests {
-		in := configWriteString(tt)
-		out, err := configParseString(in)
-		if err != nil {
-			t.Errorf("unexpected error in configParseString, %s", err)
-		}
-		if out != tt {
-			t.Errorf("configParseString returned %s (wanted %s)", strconv.QuoteToASCII(out), strconv.QuoteToASCII(tt))
-		}
+func TestConfigRoundTripBackslash(t *testing.T) {
+	tt := `hello\world`
+	in := configWriteString(tt)
+	out, err := configParseString(in)
+	if err != nil {
+		t.Errorf("unexpected error in configParseString, %s", err)
+	}
+	if out != tt {
+		t.Errorf("configParseString returned %s (wanted %s)", strconv.QuoteToASCII(out), strconv.QuoteToASCII(tt))
+	}
+}
+
+func TestConfigRoundTripTab(t *testing.T) {
+	tt := "hello\tworld"
+	in := configWriteString(tt)
+	out, err := configParseString(in)
+	if err != nil {
+		t.Errorf("unexpected error in configParseString, %s", err)
+	}
+	if out != tt {
+		t.Errorf("configParseString returned %s (wanted %s)", strconv.QuoteToASCII(out), strconv.QuoteToASCII(tt))
+	}
+}
+
+func TestConfigRoundTripUnicode(t *testing.T) {
+	tt := "Ë \" ' \\ 😅"
+	in := configWriteString(tt)
+	out, err := configParseString(in)
+	if err != nil {
+		t.Errorf("unexpected error in configParseString, %s", err)
+	}
+	if out != tt {
+		t.Errorf("configParseString returned %s (wanted %s)", strconv.QuoteToASCII(out), strconv.QuoteToASCII(tt))
 	}
 }

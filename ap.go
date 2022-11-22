36af4d03dab442579d38d1b31dbc75f662bd8004
Robert McLeod <robert@autogrow.com>
2022-11-23 11:01:30 +1300

    ensure to unsub from conn, add scan timeout

--- ap.go
+++ ap.go
@@ -1,17 +1,15 @@
 package wireless
 
-import "net"
-
 // AP represents an access point seen by the scan networks command
 type AP struct {
-	ID        int              `json:"id"`
-	RSSI      int              `json:"rssi"`
-	BSSID     net.HardwareAddr `json:"bssid"`
-	SSID      string           `json:"ssid"`
-	ESSID     string           `json:"essid"`
-	Flags     []string         `json:"flags"`
-	Signal    int              `json:"signal"`
-	Frequency int              `json:"frequency"`
+	ID        int      `json:"id"`
+	RSSI      int      `json:"rssi"`
+	BSSID     string   `json:"bssid"`
+	SSID      string   `json:"ssid"`
+	ESSID     string   `json:"essid"`
+	Flags     []string `json:"flags"`
+	Signal    int      `json:"signal"`
+	Frequency int      `json:"frequency"`
 }
 
 // APs models a collection of access points

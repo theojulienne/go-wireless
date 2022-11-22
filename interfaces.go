14eca7f85ca204b5614e0d7f8b71e668bb543946
Robert McLeod <robert@autogrow.com>
2022-11-23 11:32:08 +1300

    #21: add extra check to ensure the wireless path is in fact a dir

--- interfaces.go
+++ interfaces.go
@@ -49,7 +49,7 @@ func InterfacesFromSysfs() []string {
 
 	//  look for the wireless folder in each interfces directory to determine if it is a wireless device
 	for _, iface := range matches {
-		if _, err := os.Stat(path.Join(iface, "wireless")); err == nil {
+		if stat, err := os.Stat(path.Join(iface, "wireless")); err == nil && stat.IsDir() {
 			s = append(s, path.Base(iface))
 		}
 	}

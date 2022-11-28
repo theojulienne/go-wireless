cf064958f5d89e75cd5148769a0a6900c2eb6c32
Robert McLeod <robert@autogrow.com>
2022-11-28 20:07:49 +1300

    add extra info around errors

--- errors.go
+++ errors.go
@@ -13,13 +13,23 @@ var (
 	// ErrScanFailed is an error that happens when scanning for wifi networks fails
 	ErrScanFailed = errors.New("scan failed")
 
-	ErrSSIDNotFound  = errors.New("SSID not found")
-	ErrAuthFailed    = errors.New("auth failed")
-	ErrDisconnected  = errors.New("disconnected")
+	ErrSSIDNotFound = errors.New("SSID not found")
+
+	// ErrAuthFailed this is not actually talking about WPA but the raw 802.11 management frames.  This
+	// was originally called WEP but when security holes were found it became disused, instead passing
+	// to higher layer protocols like 802.1X (EAP) or 802.11i (WPA2).  You should never see this error.
+	ErrAuthFailed = errors.New("raw 802.11 auth failed")
+
+	ErrDisconnected = errors.New("disconnected")
+
+	// ErrAssocRejected will be given should the station fail to associate with the AP, usually due
+	// to higher level authentication protocols like WPA2 failing to authenticate a password.  You can
+	// use this to detect invalid passwords.
 	ErrAssocRejected = errors.New("assocation rejected")
-	ErrNoIdentifier  = errors.New("no id_str field found")
-	ErrInvalidEvent  = errors.New("invalid event message")
-	ErrFailBusy      = errors.New("FAIL-BUSY")
+
+	ErrNoIdentifier = errors.New("no id_str field found")
+	ErrInvalidEvent = errors.New("invalid event message")
+	ErrFailBusy     = errors.New("FAIL-BUSY")
 )
 
 // IsUseOfClosedNetworkConnectionError will return true if the error is about use of
